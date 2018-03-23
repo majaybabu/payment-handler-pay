3fe29ceefc351f0dbc4d0db1af1f893f9b30dacc
Ajay B Maddukuri <Ajay.B.Maddukuri@aexp.com>
2018-03-22 22:27:29 -0700

    second commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+function install() {
+
+    navigator.serviceWorker.register('sw.js')
+        .then(() => {
+        return navigator.serviceWorker.ready;
+})
+.then((registration) => {
+        if (!registration.paymentManager) {
+        console.log('No payment handler capability');
+        return;
+    }
+    if (!registration.paymentManager.instruments) {
+        console.log('Payment handler is not fully implemented. Cannot set the instruments.');
+        return;
+    }
+    Promise.all([
+    registration.paymentManager.instruments
+        .set('instrument-key', {
+            name: 'Chrome uses name and icon from the web app manifest',
+            enabledMethods: ['https://127.0.0.1:8080/pay3'],
+        }),
+    registration.paymentManager.instruments.set(
+        "new-card",
+        {
+            name: "Add new credit/debit card to ExampleApp",
+            enabledMethods: ["basic-card"],
+            capabilities: {
+                supportedNetworks: ['visa','mastercard','amex','discover']
+            }
+        }),
+        ])
+        .then(() => {
+            registration.paymentManager.instruments.get('instrument-key').then((instrument) => {
+            console.log('registration.scope is' + registration.scope);
+            console.log('enabledMethods is' + instrument.enabledMethods);
+
+    }).catch((error) => {
+        console.log(error);
+    });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+
+
+window.addEventListener('load', function(evt) {
+    install();
+});
+
+
---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('canmakepayment', (evt) => {
+    console.log('canmakepayment evt is ' + evt);
+    evt.respondWith(true);
+});
+
+self.addEventListener('paymentrequest', (evt) => {
+    console.log('paymentrequest evt is ' + evt);
+    evt.respondWith({
+        methodName: 'https://127.0.0.1:8080/pay3',
+        details: {
+            token: '1234567890000',
+        },
+    });
+});
+
+self.addEventListener('install', event => {
+    console.log('sw installing.........');
+});
+
+self.addEventListener('activate', event => {
+    console.log('sw now ready to handle');
+});
