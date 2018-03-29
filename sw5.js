98c7ce37fef3d759909b154e01636cb81e83cae0
madduajay <Ajay.B.Maddukuri@aexp.com>
2018-03-29 11:08:02 -0700

    update

--- sw5.js
+++ sw5.js
@@ -17,7 +17,7 @@ self.addEventListener('paymentrequest', (evt) => {
 });
 
 self.addEventListener('install', event => {
-    console.log('sw installing.........');
+    console.log('sw installing....');
 });
 
 self.addEventListener('activate', event => {
