d95a4aef8b792da93fe22f93bea5267bf1029a0c
madduajay <Ajay.B.Maddukuri@aexp.com>
2018-03-28 17:04:52 -0700

    Update

--- sw.js
+++ sw.js
@@ -5,6 +5,14 @@ self.addEventListener('canmakepayment', (evt) => {
 
 self.addEventListener('paymentrequest', (evt) => {
     console.log('paymentrequest evt is ' + evt);
+    /*event.waitUntil(
+        readDB().then(function(items) {
+            for (var i = 0; i < items.length; i++) {
+                console.log('item is ' + items[i].get())
+                //Do something
+            }
+        });
+    );*/
     evt.respondWith({
         methodName: 'https://pacific-garden-30467.herokuapp.com/pay3',
         details: {
@@ -19,4 +27,35 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
     console.log('sw now ready to handle');
+    event.waitUntil(
+        createDB()
+
+    readDB().then(function(items) {
+        for (var i = 0; i < items.length; i++) {
+            console.log('item is ' + items[i].get())
+            //Do something
+        }
+    });
+
+    );
 });
+
+
+function createDB() {
+    idb.open('cardsDB', 1, function(cardsDB) {
+        var cards = cardsDB.createObjectStore('cards', {
+            keyPath: 'id'
+        });
+        cards.put({id: 1, last4: '0001', token: '371700000000001'});
+        cards.put({id: 2, last4: '0002', token: '371700000000002'});
+        cards.put({id: 3, last4: '0003', token: '371700000000003'});
+    });
+}
+
+function readDB() {
+    idb.open('cardsDB', 1).then(function(db) {
+        var tx = db.transaction('cards', 'readonly');
+        var cards = tx.objectStore('cards');
+        return cards.getAll();
+    })
+}
