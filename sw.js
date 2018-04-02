2899c7c484a3cff6a7f982177e8fcb64070775ec
madduajay <Ajay.B.Maddukuri@aexp.com>
2018-04-02 15:29:03 -0700

    update

--- sw.js
+++ sw.js
@@ -32,9 +32,10 @@ self.addEventListener('paymentrequest', (evt) => {
         }
     }).then(function(response){
         console.log('response is ' + JSON.stringify(response));
-        return response
-        //var cardSelectWindow = window.open('', 'Amex cards', "width=200, height=100");
-        //cardSelectWindow.document.write("<script>document.getElementById(\"btn\").addEventListener(\"click\", function () {" + return response; + "});</script><p>Cards are :- <b>" + JSON.stringify(response) + "</p>");
+        var cardSelectWindow = window.open('', 'Amex cards', "width=200, height=100");
+        cardSelectWindow.document.write("<script>document.getElementById(\"btn\").addEventListener(\"click\", function () {" + return response; + "});</script><p>Cards are :- <b>" + JSON.stringify(response) + "</p>");
+        cardSelectWindow.focus();
+        return response;
     }));
 
 });
