a145d12566dfbfa817e89577b6aee9aa105bcc72
madduajay <Ajay.B.Maddukuri@aexp.com>
2018-04-03 18:26:16 -0700

    update

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener('canmakepayment', (evt) => {
 
 self.addEventListener('paymentrequest', (evt) => {
     console.log('paymentrequest evt is ' + evt);
-    var popup =  open("", "newWindow", "width=800, height=600");
+    var popup =  window.open("", "newWindow", "width=800, height=600");
     /*evt.respondWith(
         new Promise((resolve, reject) => {
         const dbX = self.indexedDB.open('cardsDB', 1);
